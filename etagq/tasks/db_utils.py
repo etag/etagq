e058842e6b4a3552ef21cf478102c4f1e86cedcd
Tyler Pearson <tdpearson@ou.edu>
2019-02-27 12:41:54 -0600

    additional testing and cleanup

--- etagq/tasks/db_utils.py
+++ etagq/tasks/db_utils.py
@@ -2,11 +2,12 @@ from celeryconfig import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT
 
 import sqlalchemy
 from sqlalchemy.engine.url import URL
+from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine, text
 
 import logging
 
-pg_db = {
+PG_DB = {
     'drivername': 'postgres',
     'username': DB_USERNAME,
     'password': DB_PASSWORD,
@@ -15,23 +16,22 @@ pg_db = {
     'port': DB_PORT
     }
 
-engine = create_engine(URL(**pg_db))
+ENGINE = create_engine(URL(**PG_DB))
 
 
 def _connect_db():
     try:
-        conn = engine.connect()
+        conn = ENGINE.connect()
         return conn
-    except sqlalchemy.exc.OperationalError as e:
+    except OperationalError as e:
         logging.error("Error with DB connection:\n{0}".format(e))
         return None
 
 
 def get_columns(table, columns):
+    """ return cursor from table with defined columns """
     query = "select :columns from :table"
     conn = _connect_db()
     if conn:
-        return conn.execute(text(query), table=table, columns=columns).fetchall() 
-    else:
-        return None
-
+        return conn.execute(text(query), table=table, columns=columns).fetchall()
+    return None
