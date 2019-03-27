d8ceefdb365d6ed18b882b2ccb36460a41786b6b
Tyler Pearson <tdpearson@ou.edu>
2019-03-27 14:10:14 -0500

    tz conversion fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='etagq',
       install_requires=[
           'sqlalchemy==1.2.18',
           'psycopg2==2.7.7',
-          'pandas',
+          'pandas>=0.24.1',
           'pytz',
       ],
       include_package_data=True,
