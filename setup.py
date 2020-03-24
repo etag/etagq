9b4ab112fe6941ce5fae9d761260b6f13829e7c1
Tyler Pearson <tdpearson@ou.edu>
2020-03-24 12:29:23 -0500

    fix syntax

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name='etagq',
       version='0.8.3',
       packages= find_packages(),
-      package_data={'etagq':['tasks/data/*']}
+      package_data={'etagq':['tasks/data/*']},
       install_requires=[
           'sqlalchemy==1.3.3',
           'psycopg2==2.8.3',
